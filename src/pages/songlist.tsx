2a9922d718f67acf4e3e36e7581d01a484767beb
SatyamadhikaryYF <satyam@yourflow.com.au>
2025-01-20 16:18:14 +0530

    songlist design changes

--- src/pages/songlist.tsx
+++ src/pages/songlist.tsx
@@ -2,32 +2,47 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { useNavigate } from "react-router-dom";
 import "../css/songlist.css"
 import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
+import { IoMdPlay, IoMdPause } from "react-icons/io";
+
+
 
 
 const Songlist = () => {
   const navigate = useNavigate();
-    return (
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
 
-      
-
-        <div className="flex flex-1 flex-col gap-4 p-4">
-
-        <div style={{display: 'flex'}} className="previous-btn">
+      <div style={{ display: 'flex', height: '3rem', alignItems: 'center'}} className="previous-btn">
         <button onClick={() => navigate("/")}><MdKeyboardDoubleArrowLeft /></button>
       </div>
 
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
 
-          {Array.from({ length: 24 }).map((_, index) => (
-            <div
-              key={index}
-              className="aspect-video h-12 w-full rounded-lg bg-muted/50"
-            />
-          ))}
-        </div> 
 
-      </ThemeProvider>
-    )
-  }
-  
-  export default Songlist
+
+        <div className="song-container">
+          <div className="play-pause-btn">
+            <IoMdPlay />
+          </div>
+          <audio src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"></audio>
+          <h1>Test</h1>
+        </div>
+
+
+
+
+        {Array.from({ length: 12 }).map((_, index) => (
+          <div className="song-container">
+            <div className="play-pause-btn">
+              <IoMdPlay />
+            </div>
+            <h1>Test</h1>
+          </div>
+        ))}
+      </div>
+
+    </ThemeProvider>
+  )
+}
+
+export default Songlist
