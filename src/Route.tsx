bcf4df7f2a85c65af6ba0fc587e5485cad72d7a0
satyamadhikary <satyamadhikary226@gmail.com>
2025-01-21 20:59:34 +0530

    Page scroll to top

--- src/Route.tsx
+++ src/Route.tsx
@@ -1,27 +1,32 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useLayoutEffect } from "react";
 import "./App.css"
 import "./index.css"
-import Page from "./pages/page"; 
+import Page from "./pages/page";
 import Home from "./pages/home";
 import About from "./pages/About";
 import Songlist from "./pages/songlist";
 
-
 function AppRoutes() {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [location.pathname]);
+
   return (
     <Routes>
-
       <Route
-       path="/"
-       element={<Page />}>
+        path="/"
+        element={<Page />}>
 
-      <Route
-       index
-       element={<Home />}/>
+        <Route
+          index
+          element={<Home />} />
 
-      <Route
-       path="/songlist"
-       element={<Songlist />}/>
+        <Route
+          path="/songlist"
+          element={<Songlist />} />
 
         <Route path="/About" element={<About />} />
       </Route>
--- src/pages/songlist.tsx
+++ src/pages/songlist.tsx
@@ -16,7 +16,7 @@ const Songlist = () => {
         <button onClick={() => navigate("/")}><MdKeyboardDoubleArrowLeft /></button>
       </div>
 
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+      <div className="flex flex-1 flex-col gap-4 p-2 pt-0">
 
 
 
