ddb1b16118dd61785f2c52bc622b8e4de7f1e356
SatyamadhikaryYF <satyam@yourflow.com.au>
2025-02-24 16:38:47 +0530

    Update app-sidebar.tsx

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -166,7 +166,7 @@ const data = {
 }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  const { setTheme } = useTheme()
+  const { setTheme, theme} = useTheme()
   return (
 <> 
     <Sidebar style={{display:"flex"}} collapsible="icon" {...props}>
@@ -174,23 +174,12 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     <div style={{padding:"6px", textAlign:"left"}}>
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" style={{background:"transparent", border:"none"}}>
+        <Button onClick={() => setTheme(theme === "light" ? "dark" : "light" )} variant="outline" size="icon" style={{background:"transparent", border:"none"}}>
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
     </DropdownMenu>
     </div>
       
