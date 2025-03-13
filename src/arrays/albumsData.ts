73beaad6e8d763d2205eea1abc3cb854023761e4
satyamadhikary <satyamadhikary226@gmail.com>
2025-03-13 13:51:43 +0530

    git remove

---
+++ src/arrays/albumsData.ts
@@ -0,0 +1,32 @@
+// albumsData.ts
+export const albumsData = [
+    {
+        id: 1,
+        name: "Ye Jawani hai Dewani",
+        imageSrc: "https://firebasestorage.googleapis.com/v0/b/flute-8592b.appspot.com/o/ye%20jawani%20h%20diwani%2Fyejawanihdiwani.jpeg?alt=media&token=7931a42f-cc5f-4dfb-8959-3f5d33ff8fa8",
+        songs: [
+            { id: 1, name: "Delhi wali Girlfriend", imageSrc: "https://firebasestorage.googleapis.com/v0/b/flute-8592b.appspot.com/o/ye%20jawani%20h%20diwani%2Fyejawanihdiwani.jpeg?alt=media&token=7931a42f-cc5f-4dfb-8959-3f5d33ff8fa8", audioSrc: "https://firebasestorage.googleapis.com/v0/b/flute-8592b.appspot.com/o/ye%20jawani%20h%20diwani%2Fsongs%2F128-Dilliwaali%20Girlfriend%20-%20Yeh%20Jawaani%20Hai%20Deewani%20128%20Kbps.mp3?alt=media&token=63c7954d-5354-4dd6-8ced-5d313e2f2e52" },
+
+            { id: 2, name: "Ghagra", imageSrc: "https://firebasestorage.googleapis.com/v0/b/flute-8592b.appspot.com/o/ye%20jawani%20h%20diwani%2Fyejawanihdiwani.jpeg?alt=media&token=7931a42f-cc5f-4dfb-8959-3f5d33ff8fa8", audioSrc: "https://firebasestorage.googleapis.com/v0/b/flute-8592b.appspot.com/o/ye%20jawani%20h%20diwani%2Fsongs%2F128-Ghagra%20-%20Yeh%20Jawaani%20Hai%20Deewani%20128%20Kbps.mp3?alt=media&token=6260cec6-47f0-497d-b428-57a5701d594b" },
+
+            { id: 3, name: "Badtameez Dil", imageSrc: "https://firebasestorage.googleapis.com/v0/b/flute-8592b.appspot.com/o/ye%20jawani%20h%20diwani%2Fyejawanihdiwani.jpeg?alt=media&token=7931a42f-cc5f-4dfb-8959-3f5d33ff8fa8", audioSrc: "https://firebasestorage.googleapis.com/v0/b/flute-8592b.appspot.com/o/ye%20jawani%20h%20diwani%2Fsongs%2F128-Badtameez%20Dil%20-%20Yeh%20Jawaani%20Hai%20Deewani%20128%20Kbps.mp3?alt=media&token=e303ec64-a898-4364-99a8-91b7df6eb7b1" },
+
+            { id: 4, name: "Balam Pichkari", imageSrc: "https://firebasestorage.googleapis.com/v0/b/flute-8592b.appspot.com/o/ye%20jawani%20h%20diwani%2Fyejawanihdiwani.jpeg?alt=media&token=7931a42f-cc5f-4dfb-8959-3f5d33ff8fa8", audioSrc: "https://firebasestorage.googleapis.com/v0/b/flute-8592b.appspot.com/o/ye%20jawani%20h%20diwani%2Fsongs%2F128-Balam%20Pichkari%20-%20Yeh%20Jawaani%20Hai%20Deewani%20128%20Kbps.mp3?alt=media&token=6b448ff1-777e-4ccb-8891-a429de6a8e1a" },
+
+            { id: 5, name: "Ilahi", imageSrc: "https://firebasestorage.googleapis.com/v0/b/flute-8592b.appspot.com/o/ye%20jawani%20h%20diwani%2Fyejawanihdiwani.jpeg?alt=media&token=7931a42f-cc5f-4dfb-8959-3f5d33ff8fa8", audioSrc: "https://firebasestorage.googleapis.com/v0/b/flute-8592b.appspot.com/o/ye%20jawani%20h%20diwani%2Fsongs%2F128-Ilahi%20-%20Yeh%20Jawaani%20Hai%20Deewani%20128%20Kbps.mp3?alt=media&token=24176539-fddf-4e6e-be5c-04c2772eeb70" },
+
+            { id: 6, name: "Kabira", imageSrc: "https://firebasestorage.googleapis.com/v0/b/flute-8592b.appspot.com/o/ye%20jawani%20h%20diwani%2Fyejawanihdiwani.jpeg?alt=media&token=7931a42f-cc5f-4dfb-8959-3f5d33ff8fa8", audioSrc: "https://firebasestorage.googleapis.com/v0/b/flute-8592b.appspot.com/o/ye%20jawani%20h%20diwani%2Fsongs%2F128-Kabira%20-%20Yeh%20Jawaani%20Hai%20Deewani%20128%20Kbps.mp3?alt=media&token=aeaef85f-f047-4fd4-b6b4-aca62decd5cd" },
+
+            { id: 7, name: "Subhanallah", imageSrc: "https://firebasestorage.googleapis.com/v0/b/flute-8592b.appspot.com/o/ye%20jawani%20h%20diwani%2Fyejawanihdiwani.jpeg?alt=media&token=7931a42f-cc5f-4dfb-8959-3f5d33ff8fa8", audioSrc: "https://firebasestorage.googleapis.com/v0/b/flute-8592b.appspot.com/o/ye%20jawani%20h%20diwani%2Fsongs%2F128-Subhanallah%20-%20Yeh%20Jawaani%20Hai%20Deewani%20128%20Kbps.mp3?alt=media&token=39a85a67-d3e9-41d5-9ecf-3cb7626ca4b1" },
+        ]
+    },
+    {
+        id: 2,
+        name: "Album 2",
+        imageSrc: "album2.jpg",
+        songs: [
+            { id: 201, name: "Song X", imageSrc: "songX.jpg", audioSrc: "songX.mp3" },
+            { id: 202, name: "Song Y", imageSrc: "songY.jpg", audioSrc: "songY.mp3" }
+        ]
+    }
+]
