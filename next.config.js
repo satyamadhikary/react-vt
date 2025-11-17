af5d354294919a028730b2bd7053cd17bdbae9f9
satyamadhikary <satyamadhikary226@gmail.com>
2025-11-17 14:26:04 +0530

    bug fix attempt

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: './src/app', // tell Next.js where the app folder is
+  },
+};
+
+module.exports = nextConfig;
